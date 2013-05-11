d6c614e4bca5111bd4cde716a3def86dbe4e9365
andrewsk <andrewsk@8c3c1cdd-9bfd-45af-9d0b-fdce53de3c23>
2013-05-11 22:13:48 +0000

    db set initial cards quantity in deck

---
+++ trunk/database/incremental_updates/06_modes_cards_quantity.sql
@@ -0,0 +1,19 @@
+﻿use lords;
+
+ALTER TABLE `lords`.`modes_cards` ADD COLUMN `quantity` INTEGER UNSIGNED NOT NULL DEFAULT 0 AFTER `card_id`;
+
+update
+modes_cards m,
+(select mode_id,card_id,count(*) as q from modes_cards group by mode_id,card_id) qq
+set m.quantity = qq.q
+where m.mode_id=qq.mode_id and m.card_id=qq.card_id;
+
+CREATE TEMPORARY TABLE tmp_min_ids (id INT);
+
+INSERT INTO tmp_min_ids
+select min(id) from modes_cards group by mode_id,card_id;
+
+delete from modes_cards
+where id not in (select id from tmp_min_ids);
+
+DROP TEMPORARY TABLE tmp_min_ids;
