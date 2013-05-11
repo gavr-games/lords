251a341c9f0a0762b2683daa3b2bb747ac985135
andrewsk <andrewsk@8c3c1cdd-9bfd-45af-9d0b-fdce53de3c23>
2013-05-11 22:59:41 +0000

    db add moat building

---
+++ trunk/database/incremental_updates/09_moat.sql
@@ -0,0 +1,12 @@
+﻿use lords;
+
+insert into buildings(name,health,radius,x_len,y_len,shape,`type`,description,log_short_name,ui_code)
+values('Ров',0,0,4,4,'0001000100011110','obstacle','Можно защитить замок.','Ров','moat');
+SET @b_id = @@last_insert_id;
+
+insert into cards(image,cost,`type`,ref,name)
+values('moat.png',0,'b',@b_id,'Ров');
+SET @c_id = @@last_insert_id;
+
+insert into modes_cards(mode_id,card_id,quantity)
+values(8,@c_id,1);
