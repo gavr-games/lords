44eb3ba30531a3f73e324f5b7163149b1cee5c34
andrewsk <andrewsk@8c3c1cdd-9bfd-45af-9d0b-fdce53de3c23>
2013-05-12 18:36:58 +0000

    db change moat to wall

--- trunk/database/incremental_updates/09_moat.sql
+++ trunk/database/incremental_updates/09_moat.sql
@@ -1,11 +1,11 @@
 ﻿use lords;
 
 insert into buildings(name,health,radius,x_len,y_len,shape,`type`,description,log_short_name,ui_code)
-values('Ров',0,0,4,4,'0001000100011110','obstacle','Можно защитить замок.','Ров','moat');
+values('Стена',0,0,4,4,'0001000100011110','obstacle','Можно защитить замок.','Стена','wall');
 SET @b_id = @@last_insert_id;
 
 insert into cards(image,cost,`type`,ref,name)
-values('moat.png',0,'b',@b_id,'Ров');
+values('wall.png',0,'b',@b_id,'Стена');
 SET @c_id = @@last_insert_id;
 
 insert into modes_cards(mode_id,card_id,quantity)
