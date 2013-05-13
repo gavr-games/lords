b9748d980487c52d605c57dc730cc94b18f557c3
andrewsk <andrewsk@8c3c1cdd-9bfd-45af-9d0b-fdce53de3c23>
2013-05-13 18:28:00 +0000

    db trees

--- trunk/database/incremental_updates/16_create_trees.sql
+++ trunk/database/incremental_updates/16_create_trees.sql
@@ -1,6 +1,5 @@
-﻿use lords;
+use lords;
 
-/*DRAFT*/
 DELIMITER $$
 
 DROP PROCEDURE IF EXISTS `lords`.`init_landscape` $$
@@ -23,27 +22,42 @@ BEGIN
   IF(building_id IS NOT NULL)THEN
   BEGIN
     DECLARE i INT;
-    DECLARE x,y INT;
+    DECLARE tree_x,tree_y INT;
     DECLARE board_building_id INT;
+    DECLARE quart INT DEFAULT 0;
 
-    SET i=qty;
-    WHILE i>0 DO
-/*get random x,y*/
+    CREATE TEMPORARY TABLE available_cells_for_trees (x INT, y INT);
 
-      INSERT INTO board_buildings(game_id,player_num,building_id,rotation,flip)VALUES (g_id,trees_p_num,building_id,0,0);
-      SET board_building_id=@@last_insert_id;
+    INSERT INTO available_cells_for_trees(x,y)
+    SELECT x,y FROM allcoords a WHERE mode_id=g_mode AND NOT ((x<=3 AND y<=3) OR (x>=16 AND y<=3) OR (x>=16 AND y>=16) OR (x<=3 AND y>=16) OR (x BETWEEN 8 AND 11) OR (y BETWEEN 8 AND 11));
+    
+    WHILE quart<4 DO
+      SET i=qty;
+      WHILE i>0 DO
+        SELECT a.x,a.y INTO tree_x,tree_y
+        FROM available_cells_for_trees a
+        WHERE quart(a.x,a.y)=quart
+        ORDER BY RAND()
+        LIMIT 1;
 
-      CALL place_building_on_board(board_building_id,x,y,0,0);
+        INSERT INTO board_buildings(game_id,player_num,building_id,rotation,flip)VALUES (g_id,trees_p_num,building_id,0,0);
+        SET board_building_id=@@last_insert_id;
 
-      IF NOT EXISTS(SELECT id FROM board WHERE game_id=g_id AND `type`<>'unit' AND ref=board_building_id) THEN /*Building not placed*/
-        DELETE FROM board_buildings WHERE id=board_building_id;
-        SET i=i+1;
-      ELSE
-        INSERT INTO board_buildings_features(board_building_id,feature_id,param) SELECT board_building_id,bf.feature_id,bf.param FROM building_default_features bf WHERE bf.building_id=building_id;
-      END IF;
+        CALL place_building_on_board(board_building_id,tree_x,tree_y,0,0);
 
-      SET i=i-1;
+        IF NOT EXISTS(SELECT id FROM board WHERE game_id=g_id AND `type`<>'unit' AND ref=board_building_id) THEN /*Building not placed*/
+          DELETE FROM board_buildings WHERE id=board_building_id;
+          SET i=i+1;
+        ELSE
+          INSERT INTO board_buildings_features(board_building_id,feature_id,param) SELECT board_building_id,bf.feature_id,bf.param FROM building_default_features bf WHERE bf.building_id=building_id;
+        END IF;
+
+        SET i=i-1;
+      END WHILE;
+      SET quart = quart+1;
     END WHILE;
+
+    DROP TEMPORARY TABLE available_cells_for_trees;
   END;
   END IF;
 
