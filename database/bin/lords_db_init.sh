5deef230cf1cf7556189275a5130bb671154992e
andiikovalov <andrewsk@ukr.net>
2018-04-04 19:43:22 +0200

    minor improvement of db scripts

--- database/bin/lords_db_init.sh
+++ database/bin/lords_db_init.sh
@@ -2,13 +2,12 @@
 
 PATH_TO_SCRIPTS=/database
 PATH_TO_BINS=/database/bin
-MYSQL_ROOT_PASS=$MYSQL_ROOT_PASSWORD
 
 $PATH_TO_BINS/create_users.sh
 
 BASELINE_SCRIPT="$PATH_TO_SCRIPTS/incremental_updates/00_baseline.sql"
 echo Running $BASELINE_SCRIPT
-mysql < $BASELINE_SCRIPT -u root --password=$MYSQL_ROOT_PASS --default-character-set=utf8
+mysql < $BASELINE_SCRIPT -u root --password=$MYSQL_ROOT_PASSWORD --default-character-set=utf8
 
 $PATH_TO_BINS/lords_db_update.sh
 
--- database/bin/lords_db_update.sh
+++ database/bin/lords_db_update.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 PATH_TO_SCRIPTS=/database
-MYSQL_ROOT_PASS=$MYSQL_ROOT_PASSWORD
 
 for entry in "$PATH_TO_SCRIPTS/incremental_updates"/*
 do
     if [ "$entry" != "$PATH_TO_SCRIPTS/incremental_updates/00_baseline.sql" ]
     then
         echo Running $entry
-        mysql < $entry -u root --password=$MYSQL_ROOT_PASS --default-character-set=utf8
+        mysql < $entry -u root --password=$MYSQL_ROOT_PASSWORD --default-character-set=utf8
     fi
 done
 
 echo Running $PATH_TO_SCRIPTS/grants.sql
-mysql < $PATH_TO_SCRIPTS/grants.sql -u root --password=$MYSQL_ROOT_PASS --default-character-set=utf8
+mysql < $PATH_TO_SCRIPTS/grants.sql -u root --password=$MYSQL_ROOT_PASSWORD --default-character-set=utf8
 
 echo DB Update done.
