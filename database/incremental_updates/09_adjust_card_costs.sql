867510fe9fc7b75f41ce60e735c01d87b3dc924e
andiikovalov <andrewsk@ukr.net>
2018-04-15 16:32:52 +0200

    adjust card costs

---
+++ database/incremental_updates/09_adjust_card_costs.sql
@@ -0,0 +1,12 @@
+use lords;
+
+update cards set cost = 10 where code = 'knight_on_foot';
+update cards set cost = 25 where code = 'knight_with_horse';
+update cards set cost = 10 where code = 'taran';
+update cards set cost = 15 where code = 'catapult';
+update cards set cost = 20 where code = 'paralich';
+update cards set cost = 20 where code = 'telekinesis';
+update cards set cost = 25 where code = 'upgrade';
+update cards set cost = 20 where code = 'polza';
+update cards set cost = 20 where code = 'vred';
+
