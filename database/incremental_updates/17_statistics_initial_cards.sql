4273f16f33d1c1c1073b3992f373ce1c5d85a8ce
andiikovalov <andrewsk@ukr.net>
2018-04-21 10:16:35 +0200

    add card colors

--- database/incremental_updates/17_statistics_initial_cards.sql
+++ database/incremental_updates/17_statistics_initial_cards.sql
@@ -25,3 +25,43 @@ values (2, @chart_p2, @measure_id, 'card_color', 'card_name', 9);
 insert into statistic_values_config(player_num, chart_id, measure_id, color, name, mode_id)
 values (3, @chart_p3, @measure_id, 'card_color', 'card_name', 9);
 
+
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS `lords`.`get_game_statistic` $$
+
+CREATE PROCEDURE `get_game_statistic`(g_id INT)
+BEGIN
+
+  SELECT
+    t.id as `tab_id`,
+    t.name as `tab_name`,
+    c.id as `chart_id`,
+    c.type as `chart_type`,
+    c.name as `chart_name`,
+    v.id as `value_id`,
+    v.value,
+    CASE vc.color
+      WHEN 'card_color' THEN
+        CASE (SELECT type FROM cards WHERE id = v.value)
+          WHEN 'u' THEN 'card_unit'
+          WHEN 'b' THEN 'card_building'
+          WHEN 'm' THEN 'card_magic'
+		END
+      ELSE vc.color
+	END as color,
+    v.name as `value_name`,
+    p.player_name
+  FROM
+    statistic_tabs t
+    JOIN statistic_charts c ON (c.tab_id = t.id)
+    JOIN statistic_values_config vc ON (vc.chart_id = c.id)
+    JOIN statistic_values v ON (v.stat_value_config_id = vc.id)
+    JOIN statistic_players p ON (p.player_num = vc.player_num)
+  WHERE v.game_id = g_id AND p.game_id = g_id
+  ORDER BY `tab_id`,`chart_id`,`value_id`;
+
+END$$
+
+DELIMITER ;
+
