3dda43f2f483d3f89ebbe3e57f079803c2a716e7
andiikovalov <andrewsk@ukr.net>
2018-04-30 11:28:54 +0200

    db shooting procedure fix

--- database/incremental_updates/32_unit_shoot_procedures.sql
+++ database/incremental_updates/32_unit_shoot_procedures.sql
@@ -38,8 +38,10 @@ BEGIN
   DECLARE mode_id INT;
   DECLARE err_code INT;
   DECLARE board_unit_id INT;
+  DECLARE shooting_unit_id INT;
   DECLARE aim_type VARCHAR(45);
   DECLARE aim_board_id INT;
+  DECLARE aim_unit_id INT;
   DECLARE distance INT;
   DECLARE dice INT;
   DECLARE chance INT;
@@ -54,21 +56,22 @@ BEGIN
   IF err_code<>0 THEN SELECT 0 AS `success`, ed.id as `error_code`, null as `error_params` FROM error_dictionary ed WHERE id=err_code;
   ELSE
     SELECT b.ref INTO board_unit_id FROM board b WHERE b.game_id=g_id AND b.x=x AND b.y=y AND b.`type`='unit' LIMIT 1;
+    SELECT bu.unit_id INTO shooting_unit_id FROM board_units bu WHERE bu.id = board_unit_id;
     SELECT b.type, b.ref INTO aim_type, aim_board_id FROM board b WHERE b.game_id = g_id AND b.x = x2 AND b.y = y2 LIMIT 1;
     IF aim_board_id IS NULL THEN
       SELECT 0 AS `success`, ed.id as `error_code`, null as `error_params` FROM error_dictionary ed WHERE ed.code='no_valid_target';
     ELSE
-      IF NOT EXISTS (SELECT id FROM shooting_params sp WHERE sp.unit_id = board_unit_id AND sp.aim_type = aim_type LIMIT 1) THEN
+      IF NOT EXISTS (SELECT id FROM shooting_params sp WHERE sp.unit_id = shooting_unit_id AND sp.aim_type = aim_type LIMIT 1) THEN
         SELECT 0 AS `success`, ed.id as `error_code`, null as `error_params` FROM error_dictionary ed WHERE ed.code='invalid_target_for_this_unit';
       ELSE
         SELECT MIN(GREATEST(ABS(shooter.x-aim.x),ABS(shooter.y-aim.y))) INTO distance
         FROM
           (SELECT b.x,b.y FROM board b WHERE b.game_id=g_id AND b.`type`='unit' AND b.ref=board_unit_id) shooter,
           (SELECT b.x,b.y FROM board b WHERE b.game_id=g_id AND b.`type`=aim_type AND b.ref=aim_board_id) aim;
-        IF distance < (SELECT MIN(distance) FROM shooting_params sp WHERE sp.unit_id = board_unit_id AND sp.aim_type = aim_type) THEN
+        IF distance < (SELECT MIN(sp.distance) FROM shooting_params sp WHERE sp.unit_id = shooting_unit_id AND sp.aim_type = aim_type) THEN
           SELECT 0 AS `success`, ed.id as `error_code`, null as `error_params` FROM error_dictionary ed WHERE ed.code='target_too_close';
         ELSE
-          IF distance > (SELECT MAX(distance) FROM shooting_params sp WHERE sp.unit_id = board_unit_id AND sp.aim_type = aim_type) THEN
+          IF distance > (SELECT MAX(sp.distance) FROM shooting_params sp WHERE sp.unit_id = shooting_unit_id AND sp.aim_type = aim_type) THEN
             SELECT 0 AS `success`, ed.id as `error_code`, null as `error_params` FROM error_dictionary ed WHERE ed.code='target_too_far';
           ELSE
 
@@ -78,8 +81,9 @@ BEGIN
             CALL cmd_unit_set_moves_left(g_id,p_num,board_unit_id);
             
             IF aim_type = 'unit' THEN
-              IF EXISTS (SELECT 1 FROM attack_bonus ab WHERE ab.mode_id=mode_id AND ab.unit_id IS NULL AND ab.aim_type='unit' AND ab.aim_id=aim_board_id) THEN
-                SELECT ab.dice_max, ab.chance, ab.damage_bonus INTO dice, chance, damage_modificator FROM attack_bonus ab WHERE ab.mode_id=mode_id AND ab.unit_id IS NULL AND ab.aim_type='unit' AND ab.aim_id=aim_board_id LIMIT 1;
+              SELECT bu.unit_id INTO aim_unit_id FROM board_units bu WHERE bu.id = aim_board_id LIMIT 1;
+              IF EXISTS (SELECT 1 FROM attack_bonus ab WHERE ab.mode_id=mode_id AND ab.unit_id IS NULL AND ab.aim_type='unit' AND ab.aim_id=aim_unit_id) THEN
+                SELECT ab.dice_max, ab.chance, ab.damage_bonus INTO dice, chance, damage_modificator FROM attack_bonus ab WHERE ab.mode_id=mode_id AND ab.unit_id IS NULL AND ab.aim_type='unit' AND ab.aim_id=aim_unit_id LIMIT 1;
                 IF get_random_int_between(1, dice) < chance THEN
                   SET miss=1;
                 END IF;
@@ -95,7 +99,7 @@ BEGIN
             IF miss=0 THEN
               SELECT sp.dice_max, sp.chance, get_random_int_between(sp.damage_modificator_min, sp.damage_modificator_max) + damage_modificator
                 INTO dice, chance, damage_modificator
-                FROM shooting_params sp WHERE sp.unit_id = board_unit_id AND sp.aim_type = aim_type AND sp.distance = distance LIMIT 1;
+                FROM shooting_params sp WHERE sp.unit_id = shooting_unit_id AND sp.aim_type = aim_type AND sp.distance = distance LIMIT 1;
 
               IF get_random_int_between(1, dice) < chance THEN
                 SET miss=1;
@@ -112,6 +116,11 @@ BEGIN
               END IF;
             ELSE
               SELECT bu.attack + damage_modificator INTO damage FROM board_units bu WHERE bu.id=board_unit_id;
+
+              CASE aim_type
+                WHEN 'unit' THEN CALL cmd_log_add_message(g_id, p_num, 'unit_shoots_unit', CONCAT_WS(';', log_unit(board_unit_id), log_unit(aim_board_id), damage));
+                ELSE CALL cmd_log_add_message(g_id, p_num, 'unit_shoots_building', CONCAT_WS(';', log_unit(board_unit_id), log_building(aim_board_id), damage));
+              END CASE;
 
               CASE aim_type
                 WHEN 'unit' THEN CALL hit_unit(aim_board_id, p_num, damage);
