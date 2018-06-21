b3cc8b1f4c050995b8c23caee273c130e05bd452
Andrii Kovalov <andrewsk@ukr.net>
2018-06-21 21:43:22 +0200

    reset baseline

--- database/incremental_updates/38_ninja_blocks_only_enemy_buildings.sql
+++
@@ -1,8 +0,0 @@
-use lords;
-
-update units_i18n set description = 'Dodges melee and range attacks with probability 1/2. Blocks opponent''s buildings when stays in their radius'
-where language_id = 1 and unit_id = (select id from units where ui_code = 'ninja' limit 1);
-
-update units_i18n set description = 'Вероятность попадания в ниндзю 1/2. Блокирует здания противника, если находится в их радиусе.'
-where language_id = 2 and unit_id = (select id from units where ui_code = 'ninja' limit 1);
-
